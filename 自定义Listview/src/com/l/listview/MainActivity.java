22f63264db0f0a9fd69461306dfbe53f1e287604
ug <524160945@qq.com>
2018-04-20 22:44:49 +0800

    下拉刷新listview

---
+++ 自定义Listview/src/com/l/listview/MainActivity.java
@@ -0,0 +1,95 @@
+package com.l.listview;
+
+import com.l.listview.view.RefreshListView;
+import com.l.listview.view.RefreshListView.OnRefreshListener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		 final RefreshListView rlv = (RefreshListView) findViewById(R.id.rlv);
+		
+		myAdapter myAdapter = new myAdapter();
+		//View header=View.inflate(this,R.layout.header,null);
+		rlv.setAdapter(myAdapter);
+		//rlv.addHeaderView(header);
+		
+		rlv.setPadding(0,0,0,0);
+		
+		rlv.setRefreshListener(new OnRefreshListener() {
+			
+			public void OnPullRefresh() {
+				
+				new Thread(){
+					
+					public void run() {
+						
+						SystemClock.sleep(3000);
+						
+						System.out.println("更新数据");
+						runOnUiThread(new Runnable() {
+							
+							public void run() {
+								rlv.completeRefresh();
+								
+							}
+						});
+						
+					};
+					
+				}.start();
+				
+			}
+		});
+		
+	}
+
+	class myAdapter extends BaseAdapter{
+
+		public int getCount() {
+			return 20;
+		}
+
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v1 = null;
+			
+			if(convertView==null){
+				
+				 v1=View.inflate(MainActivity.this,R.layout.item, null);
+				
+			}else{
+				v1=convertView;
+			}
+			
+				TextView  tv=(TextView) v1.findViewById(R.id.tv_i);
+				tv.setText("条目"+position);
+			
+		
+			
+			return v1;
+		}
+		
+	}
+
+}
---
+++ 自定义Listview/src/com/l/listview/view/RefreshListView.java
@@ -0,0 +1,232 @@
+package com.l.listview.view;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.l.listview.R;
+
+public class RefreshListView extends ListView {
+
+	private View header;
+	private ImageView arrow;
+	private ProgressBar rotate;
+	private TextView tv;
+	private int headerWidth;
+	private int headerHeight;
+
+	private final int PUll_REFRESH = 0; // 向下的刷新状态
+	private final int RELEASE_REFRESH = 1;// 松开刷新的状态
+	private final int REFRESH_ING = 2;// 坑在刷新
+
+	public RefreshListView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RefreshListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		initHeader();
+		initAnimation();
+
+	}
+
+	public RefreshListView(Context context) {
+		super(context);
+
+	}
+
+	public void initHeader() {
+
+		header = View.inflate(this.getContext(), R.layout.header, null);
+		arrow = (ImageView) header.findViewById(R.id.arrow);
+		rotate = (ProgressBar) header.findViewById(R.id.pb_rotate);
+		tv = (TextView) header.findViewById(R.id.state);
+
+		header.measure(0, 0);
+
+		headerWidth = header.getMeasuredWidth();
+		headerHeight = header.getMeasuredHeight();
+
+		header.setPadding(0, -headerHeight, 0, 0);
+
+		addHeaderView(header);
+
+	}
+
+	public void initAnimation() {
+
+		rotateDown = new RotateAnimation(-180, -360,
+				RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+				RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+		rotateDown.setDuration(300);
+		rotateDown.setFillAfter(true);
+
+		rotateUP = new RotateAnimation(0, 180,
+				RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+				RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+		rotateUP.setDuration(300);
+		rotateUP.setFillAfter(true);
+
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+		// setPadding(0, -headerHeight, 0,0);
+
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+		// TODO Auto-generated method stub
+		super.onLayout(changed, l, t, r, b);
+	}
+
+	int y0 = 0;
+	int dy = 0;
+	int currentState = 0;
+	private RotateAnimation rotateDown;
+	private RotateAnimation rotateUP;
+
+	@Override
+	public boolean onTouchEvent(MotionEvent ev) {
+
+		switch (ev.getAction()) {
+
+		case MotionEvent.ACTION_DOWN:
+
+			y0 = (int) ev.getY();
+
+			break;
+
+		case MotionEvent.ACTION_MOVE:
+			if(currentState==REFRESH_ING){
+				break;
+				}
+			
+
+			dy = (int) (ev.getY() - y0);
+
+			int padding = (int) (-headerHeight + dy);
+
+			if (padding > -headerHeight && getFirstVisiblePosition() == 0) {
+
+				if (padding > 0 && currentState == PUll_REFRESH) {
+
+					currentState = RELEASE_REFRESH;
+					refreshState();
+
+				} else if (padding < 0 && currentState == RELEASE_REFRESH) {
+					currentState = PUll_REFRESH;
+					refreshState();
+				}
+
+				header.setPadding(0, padding, 0, 0);
+				return true;
+			}
+
+			// break;
+
+		case MotionEvent.ACTION_UP:
+
+			if (currentState == PUll_REFRESH) {
+
+				header.setPadding(0, -headerHeight, 0, 0);
+			} else if (currentState == RELEASE_REFRESH) {
+				header.setPadding(0, 0, 0, 0);
+				currentState = REFRESH_ING;
+				refreshState();
+			}
+
+			// y0 = 0;
+			// dy = 0;
+			break;
+
+		default:
+			break;
+		}
+
+		return super.onTouchEvent(ev);
+	}
+
+	public void refreshState() {
+
+		switch (currentState) {
+
+		case PUll_REFRESH:
+			tv.setText("下拉刷新");
+
+			arrow.startAnimation(rotateDown);
+			break;
+
+		case RELEASE_REFRESH:
+			tv.setText("松开刷新");
+			arrow.startAnimation(rotateUP);
+			break;
+		case REFRESH_ING:
+			
+			tv.setText("正在刷新");
+			arrow.setVisibility(INVISIBLE);
+			arrow.clearAnimation();
+			rotate.setVisibility(VISIBLE);
+			
+			listener.OnPullRefresh();
+
+			break;
+
+		default:
+			break;
+
+		}
+
+	}
+	
+	public void completeRefresh(){
+		header.setPadding(0, -headerHeight, 0, 0);
+		
+		currentState=PUll_REFRESH;
+		
+		rotate.setVisibility(INVISIBLE);
+		
+		arrow.setVisibility(VISIBLE);
+		
+		tv.setText("下拉刷新"+":上次更新的时间:"+getCurrentTime());
+		
+	}
+	
+	
+	private String  getCurrentTime(){
+		
+		SimpleDateFormat format = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+		return format.format(new Date());
+		
+	}
+	
+	private OnRefreshListener listener;
+	
+	public void setRefreshListener(OnRefreshListener listener){
+		this.listener=listener;
+	}
+	
+	public interface OnRefreshListener{
+		
+		public void OnPullRefresh();
+	} 
+	
+}
